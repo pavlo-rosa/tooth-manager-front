7a628cc767d7acd8583536d262c6f629fecce94c
Pavlo Rosa <pavlo.rosa@gmail.com>
2020-08-27 14:14:56 +0200

    + init Pacient view II

--- src/containers/pacient-profile/view.js
+++ src/containers/pacient-profile/view.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { getPacient } from "../../api/pacient";
-import { Card, Avatar, Row, Col, Divider} from "antd";
-import "./styles.less";
+import { Card, Avatar, Row, Col, Divider } from "antd";
+import "./styles.scss";
+import * as moment from "moment";
 
 const { Meta } = Card;
 
@@ -32,25 +33,80 @@ class PacientProfile extends React.Component {
               />
             }
           />
-          <div id="userData" style={{ flex: 1 }}>
-            <Row gutter={8}>
-              <Col className="background-red " span={4}>
-                <span className="labelTitle">Nombre:</span>
-              </Col>
-              <Col className="" span={12}>
-                <p className="">
-                  {this.state.data.name} {this.state.data.surname} [Nombre de pila]
-                </p>
-              </Col>
-            </Row>
-            <Row>
-              <Col className="" span={4}>
-                <span className="labelTitle">Núm. Historial:</span>
-              </Col>
-              <Col className="" span={12}>
-                <div> {this.state.data.id}</div>
-              </Col>
-            </Row>
+          <div id="userData" style={{ flex: 1, display: "flex" }}>
+            <div style={{ flexBasis: "50%" }}>
+              <Row span={11}>
+                <Col className="" span={4}>
+                  <span className="labelTitle">Nombre:</span>
+                </Col>
+                <Col className="" span={20}>
+                  <span className="">
+                    {this.state.data.name} {this.state.data.surname} [Nombre de pila]
+                  </span>
+                </Col>
+              </Row>
+              <Row>
+                <Col className="" span={4}>
+                  <span className="labelTitle">Núm. Hist.:</span>
+                </Col>
+                <Col className="" span={2}>
+                  <div> {this.state.data.id}</div>
+                </Col>
+                <Col className="" span={7}>
+                  <span className="labelTitle">Mutua:&ensp;</span>
+                  <span>XXXXX</span>
+                </Col>
+                <Col className="" span={2}>
+                  <span className="labelTitle">Ref.:</span>
+                </Col>
+                <Col className="" span={4}>
+                  <div> YYYYY</div>
+                </Col>
+              </Row>
+              <Row>
+                <Col className="" span={4}>
+                  <span className="labelTitle">Edad:</span>
+                </Col>
+                <Col className="" span={9}>
+                  <div>
+                    {" "}
+                    {moment
+                      .duration(moment().diff(moment(this.state.data.birthday, "YYYY-MM-DD")))
+                      .years()}{" "}
+                    años / Hombre
+                  </div>
+                </Col>
+                <Col className="" span={3}>
+                  <span className="labelTitle">F. Nac.:</span>
+                </Col>
+                <Col className="" span={4}>
+                  <div>{moment(this.state.data.birthday, "YYYY-MM-DD").format("DD/MM/YYYY")}</div>
+                </Col>
+              </Row>
+              <Row>
+                <Col className="" span={4}>
+                  <span className="labelTitle">Telf:</span>
+                </Col>
+                <Col className="" span={12}>
+                  <div>
+                    {" "}
+                    {this.state.data.phone1}{" "}
+                    {this.state.data.phone2 ? "/ " + this.state.data.phone2 : ""}
+                  </div>
+                </Col>
+              </Row>
+            </div>
+            <Divider type="vertical" style={{ height: "100%", marginRight: "16px" }} />
+            <div style={{ flexBasis: "45%" }}>
+              <Row gutter={[8]}>
+                <Col span={2} className="background-red">
+                  col 1
+                </Col>
+                <Col span={2} className="background-blue">
+                  col 2
+                </Col>
+              </Row>
+            </div>
           </div>
         </div>
         <Divider />
